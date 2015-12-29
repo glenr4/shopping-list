41e4544a774d8b518e3416aeb6539b6f93278a9e
glenr4 <glenrutherford4@gmail.com>
2015-12-29 15:19:09 +0800

    Trying to implement add item
    Not working yet

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+	// Global variables
+	var newItem = "";
 
-	// Allow user entry and submit on Add item
+	// Allow user entry to Add item
 	$(".add").click(function(){
 		// Clear contents
 		$(".add").empty();
@@ -12,6 +14,42 @@ $(document).ready(function(){
 			})
 		);
 		addForm.appendTo(".add");
-		// console.log(".add click event fired");
+		$(".add [name='item']").focus();
 	});
+
+	// Get submitted item
+	$(".add").submit(function(){
+
+		newItem = $(".add [name='item']").val();
+		
+
+		// var newItem = $("<li class=unchecked>");
+		// newItem.innerText = "test";
+		// newItem.appendTo("#list ul");
+
+
+		// $("#list ul").append("<li class='unchecked'>Test</li>");
+
+
+		// $("#list ul").append($("<li>").attr("class","unchecked")).innerText("test");
+
+	});
+
+	// When the user hits enter, then add a new item
+	$(document).keyup(function(event){
+		// console.log("keyup" + event.keyCode);
+		if (event.keyCode === 13){
+			// console.log("enter");
+			if ($(".add").focus()){
+				// Create a new item
+				// $("#list ul").append(
+				// $("<li class=unchecked>").append(newItem));
+
+
+			};
+		};
+	});
+
+	
+
 });
