734e04f6f6c618bb312a47fdaf5c71f80f822fa2
glenr4 <glenrutherford4@gmail.com>
2015-12-30 10:51:14 +0800

    Implemented escape out of Add Item

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-	// Global variables
-	// var newItem = "";
-
 	// Create form to allow user entry to Add item
 	$(document).on("click", ".add", function(){ //Must use on() to bind to new ".add" items
 		// Clear contents
@@ -17,10 +14,6 @@ $(document).ready(function(){
 		$(".add [name='item']").focus();
 	});
 
-	// If user 'escapes' from adding an item
-
-
-
 	// Add submitted item to list
 	$(document).on("submit", ".add", function(event){ //Must use on() to bind to new ".add" items
 		// stop submit from refreshing the page
@@ -31,10 +24,6 @@ $(document).ready(function(){
 
 		// Delete Add Item
 		$("#list li:last-child").remove();
-
-		// Append new item to list (removed, use DOM object instead)
-		// $("#list ul").append(
-		// $("<li class=unchecked>").append(newItemData));	
 
 		// Append new item to list
 		var newItem = $("<li class='unchecked'>").append(newItemData);
@@ -49,6 +38,18 @@ $(document).ready(function(){
 		addItem.animate({"margin-top":"5px"}, 800);
 	});
 	
+	// If the user clicks away when adding an item
+	$(document).on("focusout", ".add", function(){
+		cancelAdd();
+	});
+
+	// If the user 'escapes' from adding an item
+	$(document).on("keyup", ".add", function(event){
+		if(event.keyCode === 27){
+			cancelAdd();
+		}
+	});
+
 	// Check off an item
 	$(document).on("click", ".unchecked", function(){
 		$(this).fadeOut(800, function(){
@@ -61,8 +62,12 @@ $(document).ready(function(){
 		$(this).attr("class", "unchecked");
 	});
 
-
+	// Delete an item
 
 
 });
 
+function cancelAdd(){
+	$(".add").empty();
+	$(".add").append("Add Item");
+}
