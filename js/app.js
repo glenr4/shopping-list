bded77789d5242f9d2f23291b6e1965441b32929
glenr4 <glenrutherford4@gmail.com>
2015-12-30 08:37:14 +0800

    Add Item complete

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	// Global variables
-	var newItem = "";
+	// var newItem = "";
 
 	// Create form to allow user entry to Add item
-	$(".add").click(function(){
+	$(document).on("click", ".add", function(){ //Must use on() to bind to new ".add" items
 		// Clear contents
 		$(".add").empty();
 		// Create a form and input field
@@ -18,14 +18,31 @@ $(document).ready(function(){
 	});
 
 	// Add submitted item to list
-	$(".add").submit(function(event){
+	$(document).on("submit", ".add", function(event){ //Must use on() to bind to new ".add" items
 		// stop submit from refreshing the page
 		event.preventDefault();
 
+		// Get new item to add
+		var newItemData = $(".add [name='item']").val();
+
+		// Delete Add Item
+		$("#list li:last-child").remove();
+
+		// Append new item to list (removed, use DOM object instead)
+		// $("#list ul").append(
+		// $("<li class=unchecked>").append(newItemData));	
+
 		// Append new item to list
-		$("#list ul").append(
-		$("<li class=unchecked>").append(
-				$(".add [name='item']").val()));	
+		var newItem = $("<li class='unchecked'>").append(newItemData);
+		newItem.hide(); //hide to allow fadeIn
+		newItem.appendTo("#list ul");
+		newItem.fadeIn(800);
+
+		// Recreate Add Item and slide down from under the appended item above
+		var addItem = $("<li class='add'>").append("Add Item");
+		addItem.css("margin-top","-35px");
+		addItem.appendTo("#list ul");
+		addItem.animate({"margin-top":"5px"}, 800);
 	});
 	
 
